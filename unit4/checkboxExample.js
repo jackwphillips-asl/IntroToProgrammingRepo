ce3de92112ac4999f949f317934db07e077d2133
Jack Phillips <jackwphillips@gmail.com>
2017-03-23 05:46:23 +0000

    Add Class23 Materials

---
+++ unit4/checkboxExample.js
@@ -0,0 +1,23 @@
+/* This program consists of a single function definition
+ * No code runs when the page is loaded. Only the function
+ * executes when the button is pressed.
+ */
+
+// Function to be executed when button is pressed. Will print out a nice message.
+function makeARecommendation() {
+	
+	// declare variables and assign to each of the checkboxes. 
+	var gil = document.getElementById("gilmoreGirls");
+	var cards = document.getElementById("houseOfCards");
+	var jones = document.getElementById("jessicaJones");
+	
+	// Create an alert with the value of the checkbox. true or false.
+	alert("Gilmore: " + gil.checked);
+	alert("Cards: " + cards.checked);
+	alert("Jessica: " + jones.checked);
+	
+	//declare a variable and save the paragraph called "message"
+	var messageParagraph = document.getElementById("message");
+	// change the text of the paragraph
+	messageParagraph.innerHTML = "Great choices!";
+}
