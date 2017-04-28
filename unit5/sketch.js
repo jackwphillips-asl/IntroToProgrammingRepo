ad90882d3a67ce5304da880171e1bc519d776c77
Jack Phillips <jackwphillips@gmail.com>
2017-04-28 11:49:05 +0100

    Add oops

--- unit5/sketch.js
+++ unit5/sketch.js
@@ -1,9 +1,15 @@
 // P5 sketch template
+var img;
 
 function setup() {
-	// Code that will be run once
+	// Code that will be run once     
+ createCanvas(640, 480);
+ img = loadImage("Papasmurf1.jpg");
+
 }
 
 function draw() {
 	// Code that will loop indefinitely
+    image(img, 0, 0, 50, 50);
+    image(img, 200, 100, 100, 100);
 }
