d08748858d0a9590a3ca72b2239b79b5c718054c
Jack Phillips <jackwphillips@gmail.com>
2018-01-01 13:09:22 +0000

    Reset for Spring 2018

--- unit4/button.js
+++
@@ -1,9 +0,0 @@
-// The main program
-alert("My program is now running");
-
-// This function will be called when 
-function buttonWasClicked() {
-	// Alert box after button was pressed
-	alert("Button was pressed!");
-}
-
--- unit5/sketch.js
+++
@@ -1,9 +0,0 @@
-// P5 sketch template
-
-function setup() {
-	// Code that will be run once     
-}
-
-function draw() {
-	// Code that will loop indefinitely
-}
