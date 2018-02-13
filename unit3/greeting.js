837858673a7942212590274dd5bf342406fdb36b
Jack Phillips <jackwphillips@gmail.com>
2018-02-13 08:21:29 +0000

    add all

---
+++ unit3/greeting.js
@@ -0,0 +1,10 @@
+// Main program
+greeting();
+greeting();
+greeting();
+
+// greeting will prompt user and welcome
+function greeting() {
+  var name = prompt("What is your name?");
+  alert("Welcome, " + name);
+}
