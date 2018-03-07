3cb750669cf655f44153884a11fc1cee88e48f6c
Jack Phillips <jackwphillips@gmail.com>
2018-03-07 13:33:38 +0000

    add kilo

--- unit3/taskSolutions/kilometerConverter.js
+++ unit3/taskSolutions/kilometerConverter.js
@@ -4,18 +4,26 @@
 */
 
 // Get input from user
-var kiloInput = prompt("Please enter a distance in kilometers.");
-var kiloNum = parseFloat(kiloInput);
-
-// Convert the input to miles
-var milesOutput = kilometers2miles(kiloNum);
-
-// Update the webpage
-var resultP = document.getElementById("resultPara");
-resultP.textContent = kiloInput + " km is " + milesOutput + " mi.";
+//var kiloInput = prompt("Please enter a distance in kilometers.");
+function mainProgram() {
+	var kiloInputBox = document.getElementById("kmbox");
+	var kiloInput = kiloInputBox.value;
+	var kiloNum = parseFloat(kiloInput);
+	
+	// Convert the input to miles
+	var milesOutput = kilometers2miles(kiloNum);
+	
+	//alert(kiloInput + " km is " + milesOutput + " mi.");
+	var resultP = document.getElementById("resultPara");
+	resultP.textContent = kiloInput + " km is " + milesOutput + " mi.";
+}
 
 // converts kilometers to miles
 function kilometers2miles(km) {
 	var miles = 0.6214 * km;
 	return miles;
 }
+
+
+// Update the webpage
+
