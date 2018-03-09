80c999f671095aa4bc4aa5064907d83eb78a009d
Jack Phillips <jackwphillips@gmail.com>
2018-03-09 08:39:12 +0000

    add unit3 work

--- unit3/practiceCode/hypo.js
+++ unit3/practiceCode/hypo.js
@@ -1,5 +1,23 @@
 function calculateHypo() {
     // Your code here
+    
+    // Grab the text boxes and get the value
+    var sideABox = document.getElementById("sideAbox");
+    var sideAString = sideABox.value;
+    var sideBBox = document.getElementById("sideBbox");
+    var sideBBoxString = sideBBox.value;
+        
+    // Convert the value to numbers
+    var sideA = parseFloat(sideAString);
+    var sideB = parseFloat(sideBString);    
+    
+    // Calculate hypo using the hypotenuse function
+    var hypoAnswer = hypotenuse(sideA, sideB);
+    
+    // Add hypotenuse answer to webpage
+    var hypoParagraph = document.getElementById("hypo");
+    hypoParagraph.textContent = hypoAnswer;
+    
 }
 
 // Returns hypotenuse given two legs of a right triangle.
