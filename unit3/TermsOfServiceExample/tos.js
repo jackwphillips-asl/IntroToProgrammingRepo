1c0d57e3c745aab7928fb4503d3c13fa4247d0d8
Jack Phillips <jackwphillips@gmail.com>
2018-03-13 06:00:41 +0000

    add tos examples

---
+++ unit3/TermsOfServiceExample/tos.js
@@ -0,0 +1,10 @@
+function submit() {
+  /* TODO: When user hits submit button, this function should check
+     if the box is checked. If it is, alert: "Form successfully submitted".
+     It not, change the tos_message to red with this code:
+        var message = document.getElementById("tos_message");
+        message.style.color = "red";
+  */
+
+
+}
