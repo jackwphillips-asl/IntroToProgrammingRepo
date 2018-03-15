5941cd80958bc48eef6bfbbfdff1eefa89d4ee1a
Jack Phillips <jackwphillips@gmail.com>
2018-03-15 05:48:16 +0000

    add tasks for Class20

---
+++ unit3/QuickMenuExample/quickMenu.js
@@ -0,0 +1,38 @@
+/*
+  TODO: Complete the functions below so that when the menu is changed, the background color
+  changes and the corresponding radio button changes. Similarly, when the radio button changes,
+  the background changes and the corresponding menu option changes.
+*/
+
+
+// --> void
+// when menu is changed, the color of background changes and the corresponding radio button changes
+function menuChanged() {
+  // Get the menu color 
+  
+  // Se the background color
+ 
+  // Select the corresponding radio button
+
+  
+}
+
+// --> void
+// when radio button changes, color of background changes and the corresponding radio button changes
+function radioClicked() {
+  var selectedColor;
+
+  // Figure out which radio is selected
+
+  // Set the background color
+  
+  // Set the menu to the corresponding color
+ 
+}
+
+
+// string: clr --> void
+// document.body.style.backgroundColor changes to clr
+function changeColor(clr) {
+  document.body.style.backgroundColor = clr;
+}
--- unit3/TermsOfServiceExample/tos.js
+++ unit3/TermsOfServiceExample/tos.js
@@ -1,10 +1,23 @@
+/* TODO: When user hits submit button, this function should check
+   if the box is checked. If it is, alert: "Form successfully submitted".
+   It not, change the tos_message to red with this code:
+      var message = document.getElementById("tos_message");
+      message.style.color = "red";
+   If message is red and then checkbox is checked and submitted, change back to black
+*/
+  
+
+// --> void
+// Checks if checkbox is checked and then responds accordingly
 function submit() {
-  /* TODO: When user hits submit button, this function should check
-     if the box is checked. If it is, alert: "Form successfully submitted".
-     It not, change the tos_message to red with this code:
-        var message = document.getElementById("tos_message");
-        message.style.color = "red";
-  */
+ 
+  
+ 
+}
 
 
+// string: clr --> void
+// Changes the tos_message element color to clr 
+function changeColor(clr) {
+ 
 }
