1acd51fb31f76b4391705cdf0dc45091c091d61e
Jack Phillips <jackwphillips@gmail.com>
2018-03-29 13:15:35 +0100

    add unit3 assessment

---
+++ unit3/unit3-Assessment/unit3Assess.js
@@ -0,0 +1,28 @@
+/***
+ * Evaluate student ability to define and invoke functions.
+ * Assess student ability to get and set properties of
+ * web elements.
+ * 
+ * Your name here, Spring 2018
+ */
+
+/**
+ * Question 1.  Area of a Circle
+ */
+
+
+/**
+ * Question 2.  Adaptable Dice
+ */
+
+
+/**
+ * Question 3.  Passcode Check
+ */
+
+var PASSWORD = "SeCrEt";
+var numAttempts = 0;
+
+function checkPass() {
+    alert("checkPass has been invoked.\nReplace this alert with code that meets the specification.");
+}
