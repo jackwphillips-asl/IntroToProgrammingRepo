fd426963b682b632c2cbce2cb47460bc21a651a5
Jack Phillips <jackwphillips@gmail.com>
2018-04-20 11:31:27 +0100

    add task 4-4

---
+++ unit4/Task4-4-Checkerboard/Task4-4-challenge.js
@@ -0,0 +1,15 @@
+var SIZE = 8
+
+function setup() {
+	//Must have same width and height to be square
+	createCanvas(600,600);
+	drawBoard(SIZE);
+}
+
+function drawPiece() {
+	// Your code here.
+}
+
+function drawBoard(numSquares, row, col) {
+	// Your code here.
+}
---
+++ unit4/Task4-4-Checkerboard/Task4-4-checkerboard.js
@@ -0,0 +1,11 @@
+var SIZE = 8
+
+function setup() {
+	//Must have same width and height to be square
+	createCanvas(600,600);
+	drawBoard(SIZE);
+}
+
+function drawBoard(numSquares) {
+	// Your code here.
+}
